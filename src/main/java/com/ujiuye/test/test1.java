437a6e455fe9a032829fe1a7087252f91aa74df1
ych521mm <1976691062@qq.com>
2020-04-12 14:24:59 +0800

    添加了一个类

---
+++ src/main/java/com/ujiuye/test/test1.java
@@ -0,0 +1,13 @@
+package com.ujiuye.test;
+
+/**
+ * @author 杨呆呆
+ * @site www.yangdaidai.com
+ * @company wu
+ * @create 2020-04-12 14:17
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("这是我用来测试git提交用的！！！");
+    }
+}
